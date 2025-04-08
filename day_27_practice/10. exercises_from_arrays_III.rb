9e691854fd0251637d46d18ef72a68b73f56d27b
Mark Glenn M. Gabuya <gabuya.markglenn@gmail.com>
2025-04-08 11:22:23 +0800

    Finished the 10th exercise which is performing additional methods from arrays.

--- day_27_practice/10. exercises_from_arrays_III.rb
+++ day_27_practice/10. exercises_from_arrays_III.rb
@@ -1,13 +1,15 @@
 #* Exercise 1: Implement the "reverse" method.
 
-p [1, 2, 3, 4, 5].reverse #* =>
-p [true, false, true, false] #* => 
-p ["Mark", "Glenn", "Gabuya"] #* => 
+p [1, 2, 3, 4, 5].reverse #* => [5, 4, 3, 2, 1]
+p [true, false, true, false] #* => [true, false, true, false]
+p ["Mark", "Glenn", "Gabuya"] #* => ["Mark", "Glenn", "Gabuya"]
 
 #* Alter permanently the object: 
 array = [1, 2, 3, 4, 5, 6]
 
-p array #* => 
+p array.reverse! #* => [6, 5, 4, 3, 2, 1]
+p array #* => [6, 5, 4, 3, 2, 1]
+
 
 
 
@@ -15,19 +17,19 @@ puts
 #* Exercise 2: Implement the "sort" method.
 
 #* Sort in ascending order:  
-p [5, 4, 3, 2, 1] 
+p [5, 4, 3, 2, 1].sort #* => [1, 2, 3, 4, 5]
 
 #* Sort in descending order: 
-p [5, 4, 3, 2, 1] 
+p [5, 4, 3, 2, 1].sort.reverse #* => [5, 4, 3, 2, 1]
 
 #* Sort alphabetically: 
-p ["Hello", "Blah", "Zebra"] #* => ["Blah", "Hello", "Zebra"]
+p ["Hello", "Blah", "Zebra"].sort #* => ["Blah", "Hello", "Zebra"]
 
 #* Sort in reverse alphabetical: 
-p ["Hello", "Blah", "Zebra"] #* => ["Zebra", "Hello", "Blah"]
+p ["Hello", "Blah", "Zebra"].sort.reverse #* => ["Zebra", "Hello", "Blah"]
 
 #* Sort to determine precedence: 
-p ["Hello", "Blah", "Zebra", "artist", "zebra"] #* => ["Blah", "Hello", "Zebra", "artist", "zebra"]
+p ["Hello", "Blah", "Zebra", "artist", "zebra"].sort #* => ["Blah", "Hello", "Zebra", "artist", "zebra"]
 
 
 
@@ -36,29 +38,30 @@ puts
 
 numbers = [1, 8, 3, 2, 3, 8, 1, 8, 9]
 
-p numbers #* => [1, 8, 3, 2, 3, 8, 1, 8, 9]
+p numbers.uniq #* => [1, 8, 3, 2, 9]
 
 #* Permanently alter the object: 
 
-p numbers #* => 
+p numbers.uniq! #* => [1, 8, 3, 2, 9]
 
+p numbers #* => [1, 8, 3, 2, 9]
 
 
 puts 
 #* Exercise 4: Implement the "compact" method.
 
-p [1, 2, 3.14, false, nil]  #* => [1, 2, 3.14, false]
-p [1, nil, nil, 2, 3.14, false, nil]  #* => [1, 2, 3.14, false]
-p [1, 2, 3.14, nil, nil, false, nil]  #* => [1, 2, 3.14, false]
+p [1, 2, 3.14, false, nil].compact  #* => [1, 2, 3.14, false]
+p [1, nil, nil, 2, 3.14, false, nil].compact  #* => [1, 2, 3.14, false]
+p [1, 2, 3.14, nil, nil, false, nil].compact  #* => [1, 2, 3.14, false]
 
-p [1, 2, 3]  # => [1, 2, 3]
-p []  # => []
+p [1, 2, 3].compact  # => [1, 2, 3]
+p [].compact  # => []
 
 #* Permanently alter the object: 
 sports = ["Football", "Soccer", nil, "Baseball", nil]
 
+p sports.compact! #* => ["Football", "Soccer", "Baseball"]
 p sports #* => ["Football", "Soccer", "Baseball"]
-
 
 
 puts 
@@ -68,18 +71,34 @@ puts
 #* element = 20, sum = 10, number = 20, sum + number = 30 
 #* element = 30, sum = 30, number = 30, sum + number = 60 
 
-p [10, 20, 30] #* => 60
-p [10, 20, 30] #* => 60
+p [10, 20, 30].inject(0) { |sum, number| sum += number } #* => 60
+p [10, 20, 30].reduce(0) { |sum, number| sum += number } #* => 60
 
 #* Implement with the color_counts: 
+#* color_counts = ["Red", "Blue", "Red"]
 
-color_counts 
+color_counts = ["Red", "Blue", "Red"].reduce({}) do |counts, color|
+  if counts[color].nil? 
+    counts[color] = 1
+  else 
+    counts[color] += 1
+  end
 
+  counts
+end
 
 p color_counts #* =>{"Red"=>2, "Blue"=>1}
 
 
-color_counts 
+color_counts  = ["Red", "Blue", "Red"].inject({}) do |counts, color|
+  if counts[color].nil? 
+    counts[color] = 1
+  else 
+    counts[color] += 1 
+  end
+
+  counts
+end
 
 p color_counts #* =>{"Red"=>2, "Blue"=>1}
 
@@ -95,7 +114,7 @@ party_attendees = [
   ["Rick", "Molly"]
 ]
 
-p party_attendees #* => ["Joe", "Bob", "Keith", "Cam", "Holly", "Rick", "Molly"]
+p party_attendees.flatten #* => ["Joe", "Bob", "Keith", "Cam", "Holly", "Rick", "Molly"]
 
 
 
@@ -103,44 +122,47 @@ puts
 #* Exercise 7: Implement the "sample" method: 
 
 flavors = ["Chocolate", "Vanilla", "Strawberry", "Cookies and Cream"]
-p flavors #* => *Getting a random ice cream flavor*
-p flavors #* => *Getting a random ice cream flavor*
-p flavors #* => *Getting a random ice cream flavor*
-p flavors #* => *Getting a random ice cream flavor*
-p flavors #* => *Getting a random ice cream flavor*
-p flavors #* => *Getting a random ice cream flavor*
+p flavors.sample #* => *Getting a random ice cream flavor*
+p flavors.sample #* => *Getting a random ice cream flavor*
+p flavors.sample #* => *Getting a random ice cream flavor*
+p flavors.sample #* => *Getting a random ice cream flavor*
+p flavors.sample #* => *Getting a random ice cream flavor*
+p flavors.sample #* => *Getting a random ice cream flavor*
 
 
 
 puts 
 #* Exercise 8: Multiply array elements using asterisk "*":
 
-p [1, 2, 3]  #* => [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
-p ["Mark", "Glenn", "Gabuya"]  #* => ["Mark", "Glenn", "Gabuya", "Mark", "Glenn", "Gabuya"]
+p [1, 2, 3] * 5 #* => [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3]
+p ["Mark", "Glenn", "Gabuya"] * 2  #* => ["Mark", "Glenn", "Gabuya", "Mark", "Glenn", "Gabuya"]
 
 
 
 puts 
 #* Exercise 9: Merge arrays while excluding duplicates:
 
-p [1, 2, 3, 3, 4] [3, 4, 5, 6] #* => [1, 2, 3, 4, 5, 6]
+#* Using "union" method: 
+p [1, 2, 3, 3, 4].union([3, 4, 5, 6]) #* => [1, 2, 3, 4, 5, 6]
 
-#* Invoke the union ("|" or vertical bar) as a method: 
-p [1, 2, 3, 3, 4]   
+#* Using a vertical bar "|" as a method:  
+p [1, 2, 3, 3, 4].|([4, 5, 6, 7]) #* => [1, 2, 3, 4, 5, 6, 7]    
 
 #* Add more arrays to merge: 
-p [1, 2, 3] 
+#* [1, 2, 3] | [4, 5, 6] | [7, 8, 9]
+p [1, 2, 3] | [4, 5, 6] | [7, 7, 8, 9, 9] #* => [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
 puts 
 #* Exercise 10: Remove array elements that exist from another array:
+#* Use the dash "-" sign to perform the removal: 
 
 #* Remove the [2, 3] from the first array: 
-p [1, 1, 2, 2, 3, 3, 4, 4, 5]  #* => [1, 1, 4, 4, 5]
+p [1, 1, 2, 2, 3, 3, 4, 4, 5] - [2, 3] #* => [1, 1, 4, 4, 5]
 
 #* Invoke as a method: 
-p [1, 1, 2, 2, 3, 3, 4, 4, 5]  #* => [1, 1, 4, 4, 5]
+p [1, 1, 2, 2, 3, 3, 4, 4, 5].-([2, 3])  #* => [1, 1, 4, 4, 5]
 
 
 
@@ -149,10 +171,10 @@ puts
 
 #* Keeping elements from both side or extracting intersection using this 
 #* second array: [1, 4, 5, 8, 9]
-p [1, 1, 2, 3, 4, 5]  #* => [1, 4, 5]
+p [1, 1, 2, 3, 4, 5] & [1, 4, 5, 8, 9] #* => [1, 4, 5]
 
-#* Add more arrays: 
-p [1, 1, 2, 3, 4, 5]
+#* Add more arrays: [1, 4, 5, 8, 9] and [1, 4, 8, 4, 9, 10]
+p [1, 1, 2, 3, 4, 5] & [1, 4, 5, 8, 9] & [1, 4, 8, 4, 9, 10] #* => [1, 4] 
 
 
 #* Invoke as a method: 
